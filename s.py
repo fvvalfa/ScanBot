c68ee7a1e61e309f8624f431725d1be7ee08488b
fvvalfa <fvv.alfa@gmail.com>
2025-11-17 20:46:33 +0500

    Last

--- s.py
+++ s.py
@@ -33,23 +33,23 @@ async def start(event):
             ),
             KeyboardButtonRow(
                 [
-                    KeyboardButton(text="PDF цвет. 150dpi"),
-                    KeyboardButton(text="PDF серый 150dpi"),
-                    KeyboardButton(text="PDF ч/б. 150dpi")
+                    KeyboardButton(text="PDF цвет. 300dpi"),
+                    KeyboardButton(text="PDF серый 300dpi"),
+                    KeyboardButton(text="PDF ч/б. 300dpi")
                 ]
             ),
             KeyboardButtonRow(
                 [
-                    KeyboardButton(text="JPEG цвет. 600dpi"),
-                    KeyboardButton(text="JPEG серый 600dpi"),
-                    KeyboardButton(text="JPEG ч/б. 600dpi")
+                    KeyboardButton(text="JPG цвет. 600dpi"),
+                    KeyboardButton(text="JPG серый 600dpi"),
+                    KeyboardButton(text="JPG ч/б. 600dpi")
                 ]
             ),
             KeyboardButtonRow(
                 [
-                    KeyboardButton(text="JPEG цвет. 300dpi"),
-                    KeyboardButton(text="JPEG серый 300dpi"),
-                    KeyboardButton(text="JPEG ч/б. 300dpi")
+                    KeyboardButton(text="JPG цвет. 300dpi"),
+                    KeyboardButton(text="JPG серый 300dpi"),
+                    KeyboardButton(text="JPG ч/б. 300dpi")
                 ]
             )
         ]
@@ -61,14 +61,13 @@ async def start(event):
         )
    # raise events.StopPropagation
 color_dict={'цвет.':'Color', 'серый':'Gray', 'ч/б.':'Lineart'}
-@bot.on(events.NewMessage(pattern="(PDF|JPEG)"))
+@bot.on(events.NewMessage(pattern="(PDF|JPG)"))
 async def echo(event):
     """Echo the user message."""
     params = event.message.text.split(" ")
-    format = params[0].lower() 
+    format = params[0].lower()
     filename = time.strftime("%Y-%m-%d_%H-%M-%S")
     color = color_dict[params[1]] 
-    
     dpi = params[2].split('dpi')[0]
     print(format, color, dpi)
     myCmd = f"scanimage --device=pixma:04A926B4_SD3009312367Q --resolution {dpi} --mode {color} --format {format} -l 0 -t 0 -x 210 -y 297 -o /srv/ScanBot/{filename}.{format}"
@@ -83,3 +82,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+#scanimage --device=pixma:04A926B4_SD3009312367Q --resolution 300 --mode Gray --format pdf -l 0 -t 0 -x 210 -y 297 -o /srv/ScanBot/file.pdf
